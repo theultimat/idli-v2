eda978cf9edac8e890dccb0f68c69f8228deca87
theultimat <theultimat@gmail.com>
2025-07-13 13:54:24 +0100

    scripts: Add underscores to encoding strings for readability.

--- scripts/isa.py
+++ scripts/isa.py
@@ -44,108 +44,111 @@ REGS_INV = {v: k for k, v in REGS.items()}
 #           predicate state is true or false.
 #   - r, s  Register range from r to s inclusive.
 #   - j     4b unsigned immediate where a zero indicates 16.
+# Underscores are only used to make the encoding strings more readable.
 ENCODINGS = {
     # Add/subtract.
-    'add':      '0000aaaabbbbcccc',     # a = b + c
-    'sub':      '0001aaaabbbbcccc',     # a = b - c
+    'add':      '0000_aaaa_bbbb_cccc',      # a = b + c
+    'sub':      '0001_aaaa_bbbb_cccc',      # a = b - c
 
     # Logical.
-    'and':      '0010aaaabbbbcccc',     # a = b & c
-    'andn':     '0011aaaabbbbcccc',     # a = b & ~c
-    'or':       '0100aaaabbbbcccc',     # a = b | c
-    'xor':      '0101aaaabbbbcccc',     # a = b ^ c
+    'and':      '0010_aaaa_bbbb_cccc',      # a = b & c
+    'andn':     '0011_aaaa_bbbb_cccc',      # a = b & ~c
+    'or':       '0100_aaaa_bbbb_cccc',      # a = b | c
+    'xor':      '0101_aaaa_bbbb_cccc',      # a = b ^ c
 
     # Indexed load/store register.
-    'ld':       '0110aaaabbbbcccc',     # a = [b + c]
-    'st':       '0111aaaabbbbcccc',     # [b + c] = a
+    'ld':       '0110_aaaa_bbbb_cccc',      # a = [b + c]
+    'st':       '0111_aaaa_bbbb_cccc',      # [b + c] = a
 
     # Load/store register range.
-    'ldm':      '1000rrrrbbbbssss',     # r..s = [b, b + 1, ...]
-    'stm':      '1001rrrrbbbbssss',     # [b, b + 1, ...] = r..s
+    'ldm':      '1000_rrrr_bbbb_ssss',      # r..s = [b, b + 1, ...]
+    'stm':      '1001_rrrr_bbbb_ssss',      # [b, b + 1, ...] = r..s
 
     # Load/store with post/pre-increment writeback.
-    'ld+':      '1010aaaabbbb0000',     # a = [b++]
-    'st+':      '1010aaaabbbb0001',     # [b++] = a
-    '+ld':      '1010aaaabbbb0010',     # a = [++b]
-    '+st':      '1010aaaabbbb0011',     # [++b] = a
+    'ld+':      '1010_aaaa_bbbb_0000',      # a = [b++]
+    'st+':      '1010_aaaa_bbbb_0001',      # [b++] = a
+    '+ld':      '1010_aaaa_bbbb_0010',      # a = [++b]
+    '+st':      '1010_aaaa_bbbb_0011',      # [++b] = a
 
     # Load/store with post/pre-decrement writeback.
-    'ld-':      '1010aaaabbbb0100',     # a = [b--]
-    'st-':      '1010aaaabbbb0101',     # [b--] = a
-    '-ld':      '1010aaaabbbb0110',     # a = [--b]
-    '-st':      '1010aaaabbbb0111',     # [--b] = a
+    'ld-':      '1010_aaaa_bbbb_0100',      # a = [b--]
+    'st-':      '1010_aaaa_bbbb_0101',      # [b--] = a
+    '-ld':      '1010_aaaa_bbbb_0110',      # a = [--b]
+    '-st':      '1010_aaaa_bbbb_0111',      # [--b] = a
 
     # Increment/decrement register.
-    'inc':      '1010aaaabbbb1000',     # a = b + 1
-    'dec':      '1010aaaabbbb1001',     # a = b - 1
+    'inc':      '1010_aaaa_bbbb_1000',      # a = b + 1
+    'dec':      '1010_aaaa_bbbb_1001',      # a = b - 1
 
     # Shift/rotate.
-    'srl':      '1010aaaabbbb1010',     # a = {1'b0, b[15:1]}
-    'sra':      '1010aaaabbbb1011',     # a = {b[15], b[15:1]}
-    'ror':      '1010aaaabbbb1100',     # a = {b[0], b[15:1]}
-    'rol':      '1010aaaabbbb1101',     # a = {b[14:0], b[15]}
+    'srl':      '1010_aaaa_bbbb_1010',      # a = {1'b0, b[15:1]}
+    'sra':      '1010_aaaa_bbbb_1011',      # a = {b[15], b[15:1]}
+    'ror':      '1010_aaaa_bbbb_1100',      # a = {b[0], b[15:1]}
+    'rol':      '1010_aaaa_bbbb_1101',      # a = {b[14:0], b[15]}
 
     # Bitwise invert.
-    'not':      '1010aaaabbbb1110',     # a = ~b
+    'not':      '1010_aaaa_bbbb_1110',      # a = ~b
 
     # Receive from UART.
-    'urx':      '1010aaaa00001111',     # a = uart()
+    'urx':      '1010_aaaa_0000_1111',      # a = uart()
 
     # Get predicate register.
-    'getp':     '1010aaaa00011111',     # a = p
+    'getp':     '1010_aaaa_0001_1111',      # a = p
 
     # Compare or read pin into predicate.
-    'eq':       '10110000bbbbcccc',     # p = b == c
-    'ne':       '10110001bbbbcccc',     # p = b != c
-    'lt':       '10110010bbbbcccc',     # p = b < c
-    'ltu':      '10110011bbbbcccc',     # p = b < c
-    'ge':       '10110100bbbbcccc',     # p = b >= c
-    'geu':      '10110101bbbbcccc',     # p = b >= c
-    'any':      '10110110bbbbcccc',     # p = |(b & c)
-    'inp':      '10110111??nn????',     # p = pin(n)
+    'eq':       '1011_0000_bbbb_cccc',      # p = b == c
+    'ne':       '1011_0001_bbbb_cccc',      # p = b != c
+    'lt':       '1011_0010_bbbb_cccc',      # p = b < c
+    'ltu':      '1011_0011_bbbb_cccc',      # p = b < c
+    'ge':       '1011_0100_bbbb_cccc',      # p = b >= c
+    'geu':      '1011_0101_bbbb_cccc',      # p = b >= c
+    'any':      '1011_0110_bbbb_cccc',      # p = |(b & c)
+    'inp':      '1011_0111_??nn_????',      # p = pin(n)
 
     # Comparison as above, but only run the following instruction if the value
     # written to p is true.
-    'eqx':      '10111000bbbbcccc',     # p = eq(b, c); cond(t)
-    'nex':      '10111001bbbbcccc',     # p = ne(b, c); cond(t)
-    'ltx':      '10111010bbbbcccc',     # p = lt(b, c); cond(t)
-    'ltux':     '10111011bbbbcccc',     # p = ltu(b, c); cond(t)
-    'gex':      '10111100bbbbcccc',     # p = ge(b, c); cond(t)
-    'geux':     '10111101bbbbcccc',     # p = geu(b, c); cond(t)
-    'anyx':     '10111110bbbbcccc',     # p = any(b, c); cond(t)
-    'inpx':     '10111111??nn????',     # p = inp(n); cond(t)
+    'eqx':      '1011_1000_bbbb_cccc',      # p = eq(b, c); cond(t)
+    'nex':      '1011_1001_bbbb_cccc',      # p = ne(b, c); cond(t)
+    'ltx':      '1011_1010_bbbb_cccc',      # p = lt(b, c); cond(t)
+    'ltux':     '1011_1011_bbbb_cccc',      # p = ltu(b, c); cond(t)
+    'gex':      '1011_1100_bbbb_cccc',      # p = ge(b, c); cond(t)
+    'geux':     '1011_1101_bbbb_cccc',      # p = geu(b, c); cond(t)
+    'anyx':     '1011_1110_bbbb_cccc',      # p = any(b, c); cond(t)
+    'inpx':     '1011_1111_??nn_????',      # p = inp(n); cond(t)
 
     # Add value to program counter.
-    'addpc':    '1100aaaa0000cccc',     # a = pc + c
+    'addpc':    '1100_aaaa_0000_cccc',      # a = pc + c
 
     # Branch and jump, optionally with link.
-    'b':        '110000001111cccc',     # pc += c
-    'j':        '110000011111cccc',     # pc = c
-    'bl':       '110000101111cccc',     # lr = pc + 1; b(c)
-    'jl':       '110000111111cccc',     # lr = pc + 1; j(c)
+    'b':        '1100_0000_1111_cccc',      # pc += c
+    'j':        '1100_0001_1111_cccc',      # pc = c
+    'bl':       '1100_0010_1111_cccc',      # lr = pc + 1; b(c)
+    'jl':       '1100_0011_1111_cccc',      # lr = pc + 1; j(c)
 
     # Read/write input and output pins.
-    'in':       '1101aaaa00nn????',     # a = pin(n)
-    'out':      '1101000001nncccc',     # pin(n, c)
-    'out1':     '1101000101nn???1',     # pin(n, 1)
-    'outp':     '1101001010nn????',     # pin(n, p)
+    'in':       '1101_aaaa_00nn_????',      # a = pin(n)
+    'out':      '1101_0000_01nn_cccc',      # pin(n, c)
+    'out1':     '1101_0001_01nn_???1',      # pin(n, 1)
+    'outp':     '1101_0010_10nn_????',      # pin(n, p)
 
     # Send over UART.
-    'utx':      '1101000011??cccc',     # uart(c)
+    'utx':      '1101_0000_11??_cccc',      # uart(c)
 
     # Set sticky carry flag.
-    'carry':    '1101000111??jjjj',     # C_in = C_out for C instructions
+    'carry':    '1101_0001_11??_jjjj',      # C_in = C_out for C instructions
 
     # Put value into predicate register.
-    'putp':     '1101001011??cccc',     # p = c & 1
+    'putp':     '1101_0010_11??_cccc',      # p = c & 1
 
     # Set compare instructions to AND/OR into P rather than replacing.
-    'andp':     '1101001111??jjjj',     # p &= q for c instructions
-    'orp':      '1101010011??jjjj',     # p |= q for c instructions
+    'andp':     '1101_0011_11??_jjjj',      # p &= q for c instructions
+    'orp':      '1101_0100_11??_jjjj',      # p |= q for c instructions
 
     # Set conditional execution state for the following instructions.
-    'cex':      '11100000mmmmmmmm',     # cond(*m)
+    'cex':      '1110_0000_mmmm_mmmm',      # cond(*m)
 }
+
+ENCODINGS = {k: v.replace('_', '') for k, v in ENCODINGS.items()}
 
 OPCODES = {
     k: int(''.join(x if x in '01' else '0' for x in v), 2)

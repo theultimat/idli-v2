0b9c0b7272d52856165dac179b7c71c6a697c3bc
theultimat <theultimat@gmail.com>
2025-08-10 15:19:24 +0100

    asm: Regenerate random tests.

---
+++ asm/tgen/alu.asm
@@ -0,0 +1,270 @@
+    .include "../test-wrapper.asm"
+
+test_main:
+    add r1, zr, 0x9cb
+    add r2, zr, 0xda5d
+    add r3, zr, 0xf073
+    add r4, zr, 0xda28
+    add r5, zr, 0xc159
+    add r6, zr, 0xee21
+    add r7, zr, 0xa9bd
+    add r8, zr, 0xca27
+    add r9, zr, 0xbac0
+    add r10, zr, 0xd0d8
+    add r11, zr, 0xc33d
+    add r12, zr, 0x890d
+    add r13, zr, 0x8d72
+    add lr, zr, 0x8096
+    add sp, zr, 0x8087
+    sub r10, r12, r4
+    or r10, sp, r4
+    not zr, r4
+    andn r5, r9, r5
+    andn sp, r10, lr
+    andn lr, r8, r7
+    xor lr, r10, 0x621f
+    add r4, r8, 0xbed4
+    inc r1, r4
+    xor r7, r4, r9
+    not lr, r12
+    inc r3, r12
+    inc r1, r10
+    sub r13, r8, r5
+    not r1, r11
+    andn r7, sp, r3
+    andn r13, lr, lr
+    not sp, r7
+    or zr, r12, r4
+    sub r11, r7, r10
+    not r5, r12
+    inc r2, r10
+    not r11, r1
+    inc r3, r11
+    sub r9, sp, r4
+    dec r1, r12
+    and r3, r6, r6
+    dec r12, r12
+    andn r13, r8, r12
+    or r11, r8, r5
+    andn zr, r2, r13
+    dec sp, r7
+    dec zr, sp
+    andn sp, r6, r5
+    not r5, r9
+    add lr, r7, r11
+    sub r3, zr, r13
+    add r9, r10, lr
+    andn r8, zr, r10
+    and r1, r11, r9
+    and sp, r5, r9
+    and r7, r6, r12
+    add r4, r3, 0xbdd6
+    or zr, r1, r3
+    dec r11, sp
+    add r3, r4, r2
+    not lr, r9
+    xor r12, r6, r3
+    andn r11, r8, lr
+    and r5, r12, r7
+    dec r8, r13
+    add r12, r9, r13
+    add r11, r9, r2
+    and r8, r2, r3
+    xor zr, r13, r10
+    add r8, lr, r9
+    sub r4, r8, r4
+    not r6, r7
+    not r6, r10
+    add r5, r5, zr
+    inc r13, r13
+    and r4, r1, zr
+    not r9, r1
+    or lr, r1, r8
+    andn r1, lr, r3
+    not r2, r1
+    and r6, r3, r5
+    not r9, r1
+    or r6, r1, r4
+    andn r2, r1, r6
+    dec r10, lr
+    andn r5, r7, r3
+    sub r7, r5, r13
+    add r12, r1, r13
+    and r5, r5, r5
+    dec r9, r6
+    xor sp, r4, zr
+    not r9, sp
+    or sp, r7, r13
+    sub r1, lr, r4
+    sub r5, r3, r12
+    or r1, lr, r13
+    dec r12, r7
+    andn r7, sp, r12
+    add lr, r7, r7
+    xor r8, r2, r3
+    or r10, zr, r13
+    and r4, r13, lr
+    add r12, r3, r10
+    add r6, r6, r1
+    or r11, r10, zr
+    add lr, r4, r5
+    not r3, lr
+    andn r3, r7, r8
+    xor lr, r7, r13
+    xor r3, r6, r13
+    inc r10, r4
+    xor r13, r2, r11
+    inc r10, sp
+    not r7, r3
+    dec r7, r4
+    and r1, r6, lr
+    inc r6, r1
+    add r5, r1, r6
+    andn sp, r7, r8
+    sub r4, r13, r7
+    xor r2, lr, zr
+    not r13, r7
+    not r11, r6
+    dec r6, r5
+    inc r5, r2
+    sub r7, zr, r4
+    dec sp, r8
+    xor zr, r7, r2
+    and r4, r7, r4
+    dec r13, r1
+    inc r5, r6
+    xor r12, r4, r9
+    not sp, r1
+    xor r5, r8, r8
+    xor r3, r2, r5
+    inc r4, r8
+    not r13, r7
+    not sp, r4
+    add r12, r10, r1
+    or zr, r10, r12
+    andn r5, r12, r4
+    or r1, r12, r12
+    xor r2, r12, r1
+    xor r5, r3, zr
+    sub r5, r10, r1
+    not sp, r2
+    andn zr, r7, r6
+    or r3, sp, r11
+    and zr, sp, r4
+    sub r7, r5, r5
+    andn r3, r3, r5
+    not r7, zr
+    inc lr, r3
+    sub r12, r4, r7
+    xor r13, r6, zr
+    not r8, r9
+    dec r9, sp
+    not r10, r1
+    xor r10, r13, r7
+    not r5, r6
+    xor sp, r11, r4
+    dec r11, r6
+    or sp, lr, r5
+    dec r11, r11
+    dec r8, r10
+    and r7, r8, r7
+    and r6, r4, r11
+    inc r2, r13
+    not r6, r11
+    or lr, r12, r4
+    inc zr, r9
+    sub r2, r12, r6
+    or r11, r1, r2
+    inc r4, r13
+    dec r11, r12
+    xor r9, r6, r10
+    xor r2, lr, r4
+    add r13, r8, zr
+    dec r3, r13
+    dec r13, r3
+    not lr, r2
+    andn r8, r8, r4
+    dec r7, r7
+    andn lr, r11, r10
+    add r12, r9, r3
+    andn r1, lr, lr
+    not r3, r9
+    andn zr, r1, r10
+    or r8, r3, r8
+    and r12, r4, r5
+    andn r3, r4, r2
+    xor r2, zr, r9
+    xor sp, zr, r13
+    not r12, zr
+    or r11, sp, r11
+    and r3, r6, r10
+    add r3, r1, r10
+    and r9, r5, zr
+    dec r9, zr
+    dec r6, r12
+    add r9, r10, lr
+    not r4, r12
+    dec r3, r10
+    sub r5, zr, r1
+    sub r3, r7, 0xb71c
+    xor r4, r4, r13
+    inc r4, r2
+    xor r6, sp, r10
+    not zr, r10
+    not sp, r9
+    or r2, r9, zr
+    xor r7, r5, lr
+    inc r7, r8
+    andn r12, r13, r6
+    add r7, r9, lr
+    and r4, r1, 0x2f15
+    add r1, r11, 0xb753
+    dec r3, r6
+    and r5, sp, r3
+    or r9, r1, r11
+    xor r7, r7, r8
+    andn r1, r6, r6
+    dec r6, lr
+    dec r3, r10
+    not r5, r12
+    not r13, lr
+    andn r2, r2, r7
+    add r9, r7, r11
+    or sp, r6, r3
+    not lr, r4
+    not r12, r11
+    dec r7, lr
+    not r1, zr
+    sub r6, r10, r9
+    dec r1, r4
+    andn r2, r7, r3
+    or r12, r9, r13
+    dec r3, r12
+    not r13, sp
+    and r7, r11, r8
+    not r6, r8
+    inc r13, r3
+    xor r12, r7, lr
+    add r7, lr, r10
+    dec r8, r12
+    or r10, r13, r5
+    and r3, r7, lr
+    inc r4, r11
+    andn r11, r5, r3
+    xor r5, r8, r2
+    inc r8, sp
+    sub r5, r2, zr
+    or r13, r5, r5
+    inc r10, r7
+    xor r3, r3, 0x3820
+    xor r3, r12, lr
+    or r8, r7, r1
+    xor r13, r11, r13
+    not r1, r10
+    andn r2, r10, r3
+    or r13, r3, r9
+    and r13, zr, r13
+    sub r9, r4, r9
+    dec r1, r1
+    add r1, zr, zr
+    j $test_ret

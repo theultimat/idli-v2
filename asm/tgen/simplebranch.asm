cc869bed634f5280393e347bcc1c8374bad8b34b
theultimat <theultimat@gmail.com>
2025-08-16 13:20:49 +0100

    asm, test: Add branch and simple ops random test.

---
+++ asm/tgen/simplebranch.asm
@@ -0,0 +1,1072 @@
+    add r1, zr, 0x22fb
+    add r2, zr, 0x7e05
+    add r3, zr, 0x3097
+    add r4, zr, 0x7320
+    add r5, zr, 0xaf7c
+    add r6, zr, 0xa2b7
+    add r7, zr, 0xcc41
+    add r8, zr, 0xf68a
+    add r9, zr, 0x2368
+    add r10, zr, 0x63cb
+    add r11, zr, 0xdb3d
+    add r12, zr, 0x66e2
+    add r13, zr, 0x2695
+    add lr, zr, 0xc6e
+    add sp, zr, 0xa510
+    getp lr
+    ne r8, r13
+    b r8
+        .org 0xf6aa
+    dec r13, r2
+    add r12, lr, r11
+    getp r2
+    sub r11, r10, lr
+    ror r6, r3
+    rol r1, r8
+    sub lr, r8, zr
+    anyx zr, lr
+    andn.t r4, lr, r4
+    dec r2, r13
+    sra r10, r1
+    sra r4, r2
+    sra r6, r5
+    b 0xd3de
+        .org 0xca96
+    addpc lr, r5
+    any r7, 0xa981
+    rol r9, lr
+    nex r9, r1
+    ltu.t r10, r1
+    geu r6, r12
+    rol r13, r12
+    or r5, r2, r6
+    addpc r4, r12
+    ltu sp, r6
+    rol r10, r5
+    ltux zr, r8
+    jl.t r9
+        .org 0xf424
+    gex r12, 0x6fd7
+    ge.t r12, r12
+    dec r8, r3
+    geux r4, r1
+    getp.t r7
+    getp r11
+    xor lr, r2, r8
+    b r5
+        .org 0xf3eb
+    geu zr, r3
+    putp r12
+    orp 4
+    or r7, r2, r6
+    ror r11, sp
+    getp r3
+    any r8, r13
+    eqx r3, r1
+    srl.t r3, r13
+    ltux r11, r11
+    addpc.t r5, r2
+    geux r4, r6
+    ne.t r3, r9
+    j r13
+        .org 0xb67b
+    ltu r8, lr
+    any r11, r8
+    carry 14
+    orp 2
+    and r9, r4, r12
+    inc r12, r5
+    ror r12, r8
+    ltu r2, 0xb80c
+    any r7, r1
+    not lr, r13
+    geu r9, 0x89d8
+    inc r3, r3
+    ltux r1, r10
+    inc.t r6, sp
+    eqx r2, r13
+    dec.t zr, r6
+    j r8
+        .org 0x3096
+    rol r4, r9
+    inc r12, r12
+    orp 0
+    eqx r13, r11
+    geu.t r7, r10
+    ne r5, r6
+    andp 7
+    jl r2
+        .org 0x7e03
+    ltx r8, r6
+    or.t r10, r10, r8
+    gex r1, r10
+    rol.t r6, r13
+    srl r7, r2
+    jl r5
+        .org 0xffbf
+    carry 4
+    add r5, zr, r4
+    inc r2, r6
+    anyx sp, lr
+    and.t r9, r3, r13
+    eq r3, r2
+    inc r4, r13
+    carry 0
+    carry 2
+    ror r8, r4
+    inc r3, r1
+    geux r6, r7
+    ne.t r11, r2
+    andn r5, r10, r7
+    getp r10
+    not r8, r8
+    sub zr, lr, r11
+    orp 3
+    geu r1, r9
+    any zr, r3
+    or r11, r6, r3
+    or r13, r10, r13
+    inc r7, r10
+    bl r8
+        .org 0xa497
+    srl r12, r10
+    and r12, r9, r12
+    lt r7, r1
+    geu r5, r8
+    geux lr, r2
+    any.t r4, r5
+    sra r7, r13
+    getp r5
+    rol r7, r5
+    ge r4, r7
+    andn r2, r13, r2
+    xor zr, r5, r2
+    sub r10, r13, r2
+    j 0xb02
+        .org 0xb02
+    dec r3, r6
+    getp lr
+    ltx r13, r10
+    inc.t r10, r6
+    carry 14
+    nex r7, r6
+    eq.t lr, r7
+    and r1, lr, r1
+    geux r13, r10
+    srl.t lr, lr
+    inc r6, zr
+    rol lr, r7
+    ne r3, r12
+    anyx lr, r9
+    andn.t r8, r7, r6
+    ne r4, r4
+    cex 3
+    andn.f zr, r1, r9
+    ne.f zr, r7
+    addpc.f zr, lr
+    andn r7, r12, r3
+    eq r8, r11
+    andn sp, r7, r2
+    eq r4, lr
+    gex r13, 0xcaeb
+    andp.t 9
+    orp 5
+    orp 8
+    nex r5, r10
+    andp.t 7
+    andp 7
+    ltx zr, r10
+    geu.t r2, r4
+    and lr, sp, r5
+    eqx r1, r2
+    ror.t r4, r4
+    cex 1
+    and.f r7, r2, 0x3e61
+    ror r7, r13
+    ltu r7, r8
+    ltux r9, 0x8250
+    eq.t r8, r12
+    any r1, r7
+    carry 3
+    or r9, r2, r3
+    anyx r7, 0x2ec2
+    andn.t r11, zr, r7
+    addpc r5, r10
+    ge r12, lr
+    putp r8
+    any r1, 0xd6ef
+    putp r5
+    add r13, r5, r7
+    rol r11, r10
+    ne lr, r11
+    nex r9, r12
+    srl.t r11, r9
+    lt r13, r12
+    xor r6, zr, r8
+    andn r5, r7, lr
+    anyx r3, r5
+    j.t r9
+        .org 0xb779
+    gex r5, 0xd03b
+    lt.t zr, r8
+    andp 15
+    eq r10, r8
+    and r6, r12, r10
+    sub r3, zr, r12
+    andn r12, r6, r6
+    not r8, r5
+    sra r12, r2
+    sub r2, r6, r13
+    addpc r12, r10
+    cex 1
+    b.t r2
+    eq r9, r6
+    carry 3
+    xor zr, r5, r13
+    eqx r12, r8
+    xor.t r10, r11, r5
+    sub r4, r6, lr
+    getp r9
+    sra r6, r10
+    lt sp, r3
+    and r11, r9, r12
+    ror r9, r5
+    andp 11
+    srl r11, r10
+    andn r4, r3, r4
+    xor r5, r10, r8
+    nex r9, r2
+    eq.t r9, r9
+    sub zr, r11, r13
+    inc r6, r8
+    andn zr, zr, r8
+    cex 1
+    inc.t r9, zr
+    b r12
+        .org 0x1333
+    inc zr, r2
+    sra sp, r8
+    cex 3
+    ltu.t r6, r8
+    andp.f 15
+    jl.t 0xccb1
+    eq lr, r5
+    geu zr, r9
+    srl r8, r5
+    ltu r13, lr
+    ge r13, r12
+    cex 2
+    srl.t r3, r9
+    and.f r2, r1, r2
+    ror r4, r8
+    or zr, r3, r1
+    not r2, r8
+    putp r7
+    or r13, r9, r4
+    gex r13, r13
+    dec.t r2, r4
+    andn r2, r12, r7
+    ltux r3, lr
+    ror.t r6, sp
+    srl r2, r3
+    ne r10, r5
+    add zr, r3, r9
+    any r7, r7
+    not r9, r7
+    ror r7, r2
+    sra r13, r8
+    cex 6
+    lt.f r6, r1
+    or.f r11, r4, r4
+    ge.f r7, r8
+    and.f r13, zr, r4
+    any.f r6, zr
+    inc.f r11, r12
+    bl 0x355
+        .org 0x16b0
+    ror r9, r9
+    or r13, r2, r12
+    ltx r2, r4
+    xor.t r13, zr, r2
+    putp r9
+    or r8, r2, r2
+    orp 15
+    carry 4
+    sub r5, r8, r11
+    inc r12, r13
+    ltx r2, r10
+    ror.t sp, r10
+    addpc r13, r2
+    addpc r4, r1
+    ltx r13, zr
+    getp.t r2
+    not r8, r6
+    dec r8, r6
+    nex r11, r1
+    rol.t r13, r12
+    cex 7
+    andn.f r10, r10, r1
+    dec.t r8, r4
+    srl.t r1, r11
+    ltu.t r7, r12
+    sra.f zr, r9
+    sra.t r8, r9
+    addpc.f r12, r6
+    anyx sp, r4
+    orp.t 7
+    addpc r2, r13
+    ne sp, r13
+    putp r1
+    sub r11, r1, r12
+    ge r5, r12
+    addpc r3, r5
+    xor r6, lr, r4
+    ge r6, zr
+    not r6, sp
+    anyx r11, r5
+    sra.t r3, zr
+    inc r3, r1
+    lt r11, r6
+    or r11, r6, r9
+    getp r8
+    dec r2, r11
+    ror r6, zr
+    xor r12, r3, r7
+    geu r8, zr
+    anyx r6, r10
+    inc.t r2, r3
+    and r9, r11, r12
+    b 0xc5db
+        .org 0xdcc0
+    sub r8, zr, r8
+    bl 0x13fb
+        .org 0xf0bd
+    cex 3
+    not.t r10, r11
+    eq.f r9, lr
+    ltu.f r5, lr
+    lt r12, r5
+    gex r11, 0x8d79
+    orp.t 11
+    dec r10, r10
+    carry 15
+    eqx sp, r3
+    lt.t r12, zr
+    inc sp, r11
+    sra r9, r8
+    geux r5, r3
+    andp.t 6
+    or r3, r8, r3
+    ge r3, r9
+    lt r12, zr
+    inc sp, sp
+    eqx r7, r1
+    sra.t r1, r11
+    getp r12
+    jl r3
+        .org 0x2905
+    andn r11, r5, r8
+    lt r9, zr
+    ltu r1, lr
+    sra r8, r6
+    sub r9, r5, r7
+    putp r6
+    bl r3
+        .org 0x5210
+    inc r5, r5
+    not r6, r13
+    anyx r11, r1
+    srl.t r11, r8
+    orp 5
+    xor r7, r2, r12
+    ne r3, lr
+    j r9
+        .org 0xadf7
+    and r1, r9, r5
+    srl zr, r8
+    andn r6, r13, zr
+    nex lr, r6
+    carry.t 2
+    srl r7, r13
+    orp 8
+    geu r11, r6
+    lt r6, r3
+    putp r9
+    ltu r5, 0x750e
+    sub r5, r4, r13
+    sra r2, r6
+    bl r9
+        .org 0x5bfc
+    ge r12, r2
+    cex 2
+    geu.f r10, r11
+    srl.f r12, r1
+    carry 14
+    bl r13
+        .org 0x5c03
+    eqx r5, r2
+    eq.t r11, r12
+    bl 0x8272
+        .org 0xde78
+    ltu r13, 0xdfbc
+    rol lr, sp
+    ltux r13, r12
+    ge.t r1, r8
+    gex r13, r4
+    add.t r11, r2, r9
+    jl 0xd99c
+        .org 0xd99c
+    anyx r2, r2
+    and.t sp, r6, zr
+    ror r3, r6
+    eqx r8, r4
+    any.t r8, r5
+    sra zr, zr
+    andn sp, r8, lr
+    any r10, r10
+    ltux zr, r12
+    or.t r3, sp, r7
+    ltx r7, r5
+    putp.t r5
+    or r9, r8, r10
+    sra r8, zr
+    gex r10, r13
+    j.t r9
+    ge r2, lr
+    putp r5
+    srl r11, r13
+    eqx r11, r9
+    or.t r1, zr, 0x2ce5
+    and r1, r2, r9
+    bl lr
+        .org 0xb834
+    any r6, r2
+    cex 6
+    carry.f 15
+    ne.t r6, r13
+    sra.f r5, zr
+    dec.t r7, r4
+    srl.f r12, r8
+    sra.t r5, r12
+    ltu r11, r13
+    dec r3, r6
+    orp 2
+    nex sp, r13
+    sub.t r1, r3, zr
+    b r11
+        .org 0xb842
+    or r13, lr, r1
+    sra r6, r6
+    ne r10, r6
+    add sp, r4, r7
+    getp sp
+    getp r12
+    addpc r5, r2
+    ltx r3, r12
+    carry.t 3
+    add sp, r3, r3
+    and r9, r3, r10
+    j 0xbc8
+        .org 0xbc8
+    ge r4, r12
+    jl 0x5fd0
+        .org 0x5fd0
+    ltx r10, r4
+    add.t lr, r11, r10
+    ltu r5, r8
+    add r13, lr, r1
+    carry 14
+    xor sp, r1, r9
+    putp r5
+    inc r7, r10
+    andn r13, r2, r1
+    cex 1
+    andn.f r11, r9, r5
+    not r2, r10
+    ge r4, r5
+    putp r3
+    ror r3, r1
+    or r11, r5, r7
+    cex 3
+    and.f r1, r6, lr
+    getp.f sp
+    srl.f r1, lr
+    nex r12, r13
+    add.t r4, r10, r3
+    eq lr, r3
+    lt zr, r9
+    geux r5, r6
+    any.t r1, r13
+    carry 10
+    srl zr, zr
+    anyx zr, r1
+    srl.t r11, r5
+    ltux r10, r12
+    any.t r7, r4
+    addpc r11, r10
+    ne r10, r2
+    any lr, r8
+    andp 14
+    ltu r10, r1
+    sub r5, r4, r11
+    j 0x30b5
+        .org 0x30b5
+    ge sp, lr
+    or r7, r12, r11
+    andn r6, r10, r12
+    any sp, r11
+    xor lr, r13, r3
+    putp r6
+    xor r9, r13, r8
+    addpc r6, r5
+    not lr, sp
+    xor r6, r11, r4
+    gex r12, r5
+    addpc.t r9, r13
+    rol r5, r13
+    ror r9, sp
+    ltu r10, r11
+    putp r8
+    orp 13
+    or zr, r8, r11
+    ne r6, lr
+    gex r5, r8
+    geu.t lr, r13
+    ltux r2, r8
+    ne.t sp, zr
+    sub r4, r3, r12
+    eqx r5, r3
+    any.t r1, r8
+    carry 13
+    putp r13
+    carry 15
+    eqx sp, r1
+    or.t r1, r2, r7
+    inc r1, r2
+    ltux r4, r5
+    addpc.t r2, r4
+    ne r9, r5
+    add r7, sp, r13
+    orp 1
+    ror r4, r13
+    ge r7, r6
+    any sp, r9
+    jl 0x35fc
+        .org 0x35fc
+    gex r10, r8
+    ne.t r12, zr
+    and r8, r9, r10
+    not sp, r7
+    rol r6, r11
+    putp r6
+    orp 5
+    any r13, r3
+    j r6
+        .org 0x802
+    geux r10, r7
+    or.t sp, r6, r7
+    addpc sp, r13
+    getp r2
+    and r13, r11, r11
+    addpc zr, r4
+    ror r6, r4
+    nex r13, 0xda8
+    dec.t r6, r8
+    eqx r9, r1
+    ge.t r6, r12
+    geu lr, r12
+    xor r8, sp, r10
+    ltu r1, r7
+    not r7, lr
+    and lr, lr, r2
+    srl r7, r3
+    carry 14
+    rol r11, r11
+    srl lr, r10
+    ge lr, r7
+    getp sp
+    getp zr
+    any r5, r1
+    putp r2
+    xor r3, r8, r9
+    ge r5, r3
+    any r12, r10
+    dec lr, zr
+    geu r3, r1
+    geux r13, r1
+    andn.t r7, r3, r6
+    geu r5, r1
+    putp r12
+    orp 1
+    nex zr, r9
+    rol.t r13, sp
+    addpc r5, r3
+    and lr, zr, r6
+    bl r13
+        .org 0x82c
+    add lr, r9, r9
+    or lr, r9, r13
+    ge r6, r4
+    not r10, r13
+    xor lr, r4, r9
+    rol r5, r1
+    any r13, zr
+    geux r12, r9
+    dec.t r10, r11
+    sra r8, r7
+    cex 7
+    eq.t r9, r8
+    ne.f r8, r3
+    orp.t 5
+    ge.f r5, r4
+    andp.t 10
+    geu.t r4, lr
+    putp.f r1
+    bl r8
+        .org 0x283e
+    ltu r7, r4
+    ror r5, r6
+    or r3, r10, r13
+    dec r4, r3
+    sub r11, sp, lr
+    anyx r4, r11
+    dec.t lr, r4
+    addpc r5, r11
+    and r2, sp, r12
+    anyx r3, r4
+    srl.t r11, r11
+    lt r9, r3
+    inc lr, r11
+    anyx r10, r11
+    getp.t sp
+    or zr, r4, zr
+    anyx r2, r11
+    geu.t r12, r4
+    jl lr
+        .org 0x7be2
+    or r6, r1, r5
+    b r5
+        .org 0x9bea
+    geux zr, r1
+    and.t r4, zr, r12
+    addpc r8, r2
+    dec sp, r5
+    or zr, r3, r1
+    putp r12
+    or r6, r3, 0xd464
+    inc r4, zr
+    geu r9, r4
+    putp r7
+    geux lr, r10
+    xor.t r8, r5, r13
+    sra r1, r3
+    and r8, r11, r3
+    andp 11
+    dec r2, r13
+    ge r7, 0x35d2
+    geu r12, r1
+    andp 15
+    getp r4
+    anyx r6, r10
+    sra.t r2, r12
+    orp 15
+    j r6
+        .org 0xdc67
+    sra r9, r12
+    geux r13, r1
+    putp.t r13
+    getp r10
+    dec r5, r9
+    eq r8, r3
+    any r3, r11
+    inc zr, sp
+    and sp, r4, lr
+    or lr, r9, r1
+    carry 7
+    ge r12, r6
+    eq r12, zr
+    andn r3, r5, r7
+    add r6, r11, r7
+    cex 2
+    carry.t 7
+    ne.t r6, r11
+    orp 13
+    addpc r9, r13
+    geu r13, r5
+    andp 13
+    dec r9, r11
+    anyx lr, r5
+    andp.t 12
+    and r5, r7, r11
+    anyx r5, r9
+    orp.t 1
+    cex 6
+    rol.f r5, lr
+    ltu.f r8, r7
+    ltu.f r4, r8
+    getp.f r12
+    ror.f zr, r11
+    add.t r3, zr, r2
+    bl 0x68ee
+        .org 0x4579
+    srl r4, r5
+    nex r10, r11
+    getp.t r7
+    andn r11, r1, r5
+    eq r2, r3
+    lt r13, r1
+    ne r7, r7
+    inc r6, r10
+    and r4, r9, r7
+    dec r11, r3
+    cex 7
+    andn.f r8, r1, r11
+    inc.f r13, r10
+    andp.t 12
+    ge.f r12, r4
+    or.t r10, r10, r5
+    ge.f r2, r2
+    or.f r1, r10, zr
+    add r2, r8, r12
+    not r11, r11
+    sra zr, r13
+    putp lr
+    or r3, r7, r6
+    and r6, r9, lr
+    eq r9, lr
+    eq r2, r2
+    getp r1
+    ne sp, r7
+    xor r8, r10, r10
+    rol r2, r12
+    lt r5, r10
+    cex 4
+    inc.f r8, r3
+    andp.f 4
+    inc.t r11, lr
+    carry.t 7
+    getp r2
+    eqx r7, lr
+    add.t r10, r11, zr
+    sub r8, r10, r10
+    andn r5, sp, r8
+    sra r7, r13
+    geu r5, zr
+    rol r5, r3
+    cex 6
+    andn.f r11, r1, r13
+    and.t r4, r11, r7
+    orp.t 11
+    sub.t r13, r2, zr
+    or.f r9, r2, r6
+    inc.f r3, r1
+    j r6
+        .org 0x5880
+    andn r7, r7, r3
+    andp 3
+    ltu r5, r7
+    ge r6, zr
+    geu r6, zr
+    putp lr
+    getp r11
+    lt r7, r1
+    ltx r13, lr
+    not.t sp, r5
+    dec r10, r5
+    add r7, lr, r7
+    eq r2, r11
+    putp lr
+    eqx r3, r3
+    and.t lr, r7, r3
+    lt r13, r8
+    eq sp, r13
+    eqx r11, r6
+    andn.t zr, r8, r5
+    any lr, r11
+    ge r13, r7
+    rol r11, r11
+    ge r1, r7
+    jl 0x4d2
+        .org 0x4d2
+    rol r9, r9
+    b r7
+        .org 0xe15f
+    lt r12, r4
+    addpc r7, 0xdaa
+    orp 13
+    ror r13, r9
+    ltux r13, r10
+    dec.t r4, r8
+    addpc r2, r12
+    not r13, r9
+    carry 10
+    andn r6, r11, r6
+    not r1, r2
+    geu r12, r2
+    lt r10, r4
+    ltx sp, r12
+    xor.t r1, r2, r4
+    geu r2, r2
+    srl r13, r11
+    andn sp, zr, r11
+    orp 6
+    srl r10, r3
+    putp r8
+    dec r3, r4
+    putp r12
+    addpc r9, r4
+    nex r5, r3
+    eq.t r5, r7
+    ltux r8, r6
+    and.t r11, r9, r11
+    eq r7, r6
+    ror r13, r8
+    sra r5, r7
+    not r11, r5
+    j 0x7936
+        .org 0x7936
+    sub r11, r12, r9
+    ltu r6, r11
+    anyx r2, r2
+    putp.t r8
+    jl 0xa44a
+        .org 0xa44a
+    lt r13, r4
+    ltux r2, r3
+    not.t r8, lr
+    andn r11, r8, r1
+    putp r5
+    carry 15
+    geux r13, r7
+    rol.t zr, r7
+    any sp, r8
+    ltx r7, r4
+    orp.t 7
+    inc r12, r4
+    eqx sp, r11
+    sub.t r9, r2, r5
+    inc r12, lr
+    or r6, r12, r3
+    inc r7, r9
+    add r12, r5, r10
+    rol r11, lr
+    ge sp, 0xad95
+    nex sp, r2
+    srl.t r4, r6
+    carry 12
+    getp r2
+    orp 3
+    carry 2
+    getp r10
+    gex r9, r8
+    putp.t r2
+    gex r12, r11
+    addpc.t r9, r12
+    ge r2, r3
+    eqx lr, r13
+    sra.t sp, r2
+    xor r9, r12, r1
+    and r10, r10, zr
+    lt r11, r11
+    eq r13, zr
+    cex 2
+    sra.t r5, lr
+    carry.t 15
+    anyx r5, r5
+    putp.t lr
+    getp r11
+    gex r3, r6
+    ne.t sp, r4
+    geu r2, r6
+    addpc r4, r5
+    nex sp, r13
+    add.t r5, r9, r9
+    inc r6, r3
+    add r4, r11, r9
+    getp r12
+    sub r5, r6, r10
+    addpc r8, r9
+    not lr, r12
+    getp r5
+    ne lr, r13
+    ltux r4, r7
+    getp.t r10
+    rol r5, r13
+    nex zr, r1
+    lt.t r5, r7
+    getp r10
+    nex r2, r2
+    and.t r2, r13, r1
+    ltux zr, r10
+    eq.t r10, lr
+    or r5, r6, r12
+    and r10, zr, r6
+    and r12, r6, r3
+    ne r1, r11
+    nex r7, r9
+    j.t 0x8a00
+        .org 0x8a00
+    inc r11, r12
+    eqx sp, r3
+    inc.t r5, r8
+    ge r9, zr
+    eq r10, r12
+    dec zr, r2
+    addpc lr, r3
+    nex r13, zr
+    ne.t r5, r13
+    j r1
+        .org 0x1e99
+    anyx r11, r4
+    putp.t r12
+    andn r8, r13, 0xb82f
+    or r5, r4, lr
+    or r12, sp, lr
+    geu r1, r4
+    b 0xfe3d
+        .org 0x1cde
+    addpc r4, r10
+    nex sp, zr
+    sub.t r6, zr, r13
+    carry 14
+    eq lr, r4
+    and r6, r8, r11
+    ltu r11, r6
+    not r2, r8
+    geu r8, r10
+    rol r11, sp
+    bl r1
+        .org 0x3b81
+    andp 11
+    inc r6, r7
+    sra r12, r8
+    nex zr, r10
+    rol.t sp, r13
+    orp 1
+    not r13, lr
+    srl r12, r3
+    eqx r13, r8
+    lt.t r8, r13
+    ne r7, r4
+    jl 0x5490
+        .org 0x5490
+    and r11, r2, lr
+    andn r10, r11, r9
+    add r4, zr, r12
+    carry 2
+    jl r6
+        .org 0xe9e3
+    xor r2, r9, lr
+    addpc r8, r12
+    xor r5, r4, r6
+    orp 5
+    ltx sp, r9
+    ne.t r1, r4
+    andp 0
+    andn r5, r13, r9
+    cex 6
+    srl.t r11, lr
+    rol.t sp, r2
+    ror.t zr, r6
+    orp.t 9
+    inc.f r13, sp
+    srl.f r13, r6
+    not r9, sp
+    anyx r5, r4
+    rol.t r9, r3
+    ror r13, r1
+    eq r5, r5
+    cex 3
+    jl.f r13
+    andp.f 15
+    putp.t r10
+    lt r5, r8
+    lt r8, r7
+    andn r13, sp, lr
+    anyx zr, r6
+    sra.t r13, lr
+    any r12, r9
+    gex r6, r13
+    any.t r2, r12
+    gex r2, r6
+    carry.t 1
+    any r2, r11
+    any r3, r5
+    ltu sp, r7
+    ge r3, 0x3edc
+    eq r4, r12
+    geu r6, r1
+    sub r1, r7, r4
+    eq r13, r8
+    carry 7
+    rol r8, r6
+    ne r10, zr
+    cex 5
+    lt.f r9, r9
+    or.t r3, r13, r2
+    and.t r3, r8, lr
+    getp.f lr
+    xor.f r1, r1, r9
+    add lr, r10, r2
+    cex 1
+    j.t 0x5448
+        .org 0x5448
+    srl sp, r9
+    xor r11, r11, 0xc5f1
+    inc r11, r11
+    any sp, r12
+    and r13, r2, r12
+    andp 2
+    addpc r2, r2
+    sub r13, r2, r13
+    lt r12, r8
+    ltx r7, lr
+    carry.t 9
+    lt zr, r8
+    j r3
+        .org 0x5085
+    getp r9
+    geux r12, lr
+    carry.t 11
+    lt r9, r11
+    ltu r2, r11
+    ge lr, r9
+    eqx lr, r9
+    geu.t r3, lr
+    not sp, lr
+    putp r12
+    or r6, r3, r12
+    lt r1, r6
+    xor r7, r8, r1
+    lt r2, r7
+    ltx r3, zr
+    jl.t r7
+    add r9, r7, 0x3d1f
+    xor sp, r2, r2
+    eq r5, r7
+    getp r3
+    carry 4
+    ltu r7, r1
+    ne r3, r8
+    or r1, zr, r9
+    sra r6, r1
+    ne r3, r12
+    geu r8, r12
+    lt r5, r4
+    getp r8
+    add sp, r11, lr
+    rol r5, r9
+    ltx r12, r11
+    dec.t r10, r13
+    eqx r9, r13
+    addpc.t r2, r12
+    any r6, r2
+    utx 0x40
+    utx 0x40
+    utx 0x45
+    utx 0x4e
+    utx 0x44
+    utx 0x40
+    utx 0x40
+    utx zr
+    b 0xffff

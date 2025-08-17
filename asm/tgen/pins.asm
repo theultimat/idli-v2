daee89c8285b8dd6c653a32e7ed2b1d7ff54644c
theultimat <theultimat@gmail.com>
2025-08-17 12:07:58 +0100

    asm, test: Add random IO pin test.

---
+++ asm/tgen/pins.asm
@@ -0,0 +1,1026 @@
+    add r1, zr, 0xbc32
+    add r2, zr, 0xad0e
+    add r3, zr, 0xdf2a
+    add r4, zr, 0x9d69
+    add r5, zr, 0x834f
+    add r6, zr, 0xb1a4
+    add r7, zr, 0xa04b
+    add r8, zr, 0xecbe
+    add r9, zr, 0x5dae
+    add r10, zr, 0x65da
+    add r11, zr, 0x9e41
+    add r12, zr, 0xa00b
+    add r13, zr, 0xf5de
+    add lr, zr, 0x343a
+    add sp, zr, 0x5fe4
+    putp zr
+    inpx 0
+    inp.t 0
+    outp 2
+    outp 1
+    outp 1
+    inp 2
+    out 1, r5
+    in lr, 3
+    outp 0
+    outp 0
+    inpx 0
+    inp.t 2
+    out 0, r10
+    outp 1
+    inp 1
+    inpx 1
+    out.t 1, r2
+    in r2, 2
+    in sp, 1
+    out 0, r6
+    in r4, 2
+    outn 2, r10
+    inpx 3
+    out.t 2, r10
+    out 3, r5
+    outp 3
+    in r13, 2
+    outp 3
+    in sp, 3
+    out 0, r1
+    outp 3
+    in r7, 3
+    outp 0
+    inpx 1
+    outn.t 0, r12
+    inp 2
+    outp 1
+    inp 2
+    in r10, 3
+    out 2, r4
+    in sp, 3
+    out 3, lr
+    inp 0
+    out 1, r10
+    in r6, 1
+    outp 2
+    in lr, 1
+    out 3, lr
+    inp 0
+    out 2, r4
+    inp 0
+    inp 2
+    in r9, 1
+    out 0, r13
+    outp 0
+    outp 0
+    out 0, r13
+    outn 3, r5
+    out 0, r2
+    in r3, 2
+    inpx 0
+    outn.t 0, r4
+    outn 1, 0x50b7
+    outp 0
+    out 0, r4
+    in r6, 3
+    in r1, 1
+    inp 1
+    inpx 1
+    outn.t 2, r4
+    in r7, 0
+    in r3, 3
+    outn 0, r5
+    outn 1, 0x9652
+    outn 1, 0x4aff
+    in r13, 1
+    in r7, 1
+    out 2, r6
+    in r9, 1
+    inpx 1
+    outp.t 0
+    inpx 1
+    outp.t 1
+    inpx 2
+    out.t 3, r10
+    in sp, 0
+    inpx 3
+    outp.t 3
+    inpx 3
+    in.t r12, 2
+    inp 2
+    inp 3
+    outp 0
+    out 3, r7
+    outn 1, r10
+    in r12, 2
+    out 1, r13
+    in r10, 3
+    out 2, zr
+    out 3, lr
+    out 3, r6
+    inp 3
+    outn 1, zr
+    inpx 0
+    out.t 1, r4
+    outp 0
+    outn 2, zr
+    outn 3, r11
+    out 0, r8
+    out 1, r6
+    inpx 3
+    in.t r12, 1
+    outp 3
+    outn 0, r5
+    in r12, 3
+    outn 0, r6
+    outn 3, r5
+    outp 0
+    inp 2
+    in r1, 0
+    outn 3, r3
+    inpx 2
+    in.t r6, 3
+    inpx 0
+    outn.t 3, r7
+    outn 2, r3
+    inpx 3
+    out.t 0, r13
+    outp 1
+    in r13, 3
+    inpx 2
+    out.t 1, r7
+    inp 2
+    in lr, 3
+    in r10, 0
+    inp 2
+    in r5, 2
+    out 0, r1
+    out 1, r12
+    outn 1, r7
+    inpx 1
+    outn.t 2, r3
+    in r5, 2
+    outn 1, r11
+    outn 2, r1
+    outp 0
+    outp 0
+    inpx 3
+    in.t r2, 0
+    outn 1, r7
+    in r8, 2
+    outp 3
+    outn 3, zr
+    outn 3, r8
+    in r7, 0
+    in zr, 2
+    outn 3, r11
+    in r2, 3
+    in r12, 2
+    in r8, 3
+    out 0, lr
+    in lr, 2
+    outn 1, r2
+    in r6, 1
+    inpx 0
+    in.t r10, 2
+    inp 2
+    in r12, 0
+    out 3, r11
+    inp 3
+    outn 0, r8
+    outn 3, r5
+    inp 2
+    outp 1
+    inp 2
+    inpx 1
+    outp.t 1
+    inpx 3
+    outn.t 0, zr
+    outp 1
+    outn 3, lr
+    inp 1
+    inp 1
+    inp 0
+    in r3, 3
+    in r11, 3
+    outp 1
+    out 1, r1
+    outp 2
+    outp 0
+    outp 1
+    in r1, 0
+    inpx 1
+    outp.t 3
+    out 2, zr
+    outn 3, r12
+    inp 0
+    out 3, r5
+    out 2, r6
+    inp 0
+    outn 0, r3
+    inpx 2
+    inp.t 2
+    outn 2, r5
+    inpx 3
+    out.t 0, lr
+    inp 1
+    inpx 3
+    outn.t 3, r12
+    out 2, lr
+    outp 3
+    inp 2
+    outp 1
+    outn 3, r8
+    in zr, 3
+    outn 2, r5
+    in r2, 1
+    inp 1
+    inpx 3
+    inp.t 2
+    out 2, 0xdd88
+    inp 3
+    outn 2, r10
+    outp 2
+    out 3, r9
+    out 3, r5
+    outp 2
+    in lr, 2
+    inp 3
+    inpx 2
+    inp.t 0
+    in sp, 2
+    inp 1
+    inpx 0
+    out.t 0, r13
+    outn 3, r9
+    outp 1
+    inp 1
+    outn 0, r3
+    inp 3
+    out 3, r6
+    inpx 0
+    in.t lr, 2
+    outp 2
+    outp 0
+    inpx 1
+    outn.t 1, r2
+    inpx 1
+    out.t 0, r9
+    outn 1, r3
+    inpx 0
+    out.t 2, lr
+    inp 3
+    out 2, r9
+    outn 2, r8
+    inp 0
+    outn 1, lr
+    outn 0, r4
+    outp 1
+    in r5, 0
+    outn 1, 0x35d8
+    inpx 1
+    in.t r7, 1
+    out 1, r5
+    outn 0, zr
+    outn 3, lr
+    outn 0, r11
+    inpx 1
+    outn.t 2, r12
+    outp 3
+    outp 2
+    outn 2, zr
+    inp 0
+    inp 2
+    outp 1
+    inp 2
+    outn 2, r3
+    inpx 2
+    outp.t 3
+    in r2, 1
+    outp 1
+    inpx 1
+    inp.t 3
+    in r2, 0
+    inpx 0
+    inp.t 3
+    inpx 3
+    in.t r10, 1
+    out 2, r11
+    inpx 2
+    in.t r4, 0
+    out 0, r9
+    in r5, 3
+    inp 3
+    in r12, 1
+    outp 3
+    inp 2
+    outp 1
+    outp 1
+    inpx 3
+    inp.t 3
+    outn 3, r11
+    inp 3
+    outn 0, r2
+    outn 3, r1
+    outp 2
+    inp 2
+    out 2, r3
+    inpx 1
+    outp.t 3
+    outn 1, lr
+    out 2, r8
+    inp 2
+    in r12, 0
+    outp 0
+    outn 3, r7
+    outn 0, r9
+    in r8, 3
+    out 3, r9
+    in r11, 0
+    in r6, 3
+    outp 2
+    inpx 2
+    out.t 0, r13
+    out 3, 0xd099
+    inpx 1
+    outn.t 1, r7
+    outn 2, 0xc50
+    outn 1, r11
+    inp 3
+    inpx 1
+    inp.t 3
+    inpx 3
+    outn.t 1, r7
+    outp 2
+    inp 2
+    outn 3, r10
+    out 0, r5
+    outn 1, r13
+    out 1, r8
+    out 1, zr
+    in r13, 0
+    in r7, 0
+    outn 2, lr
+    inpx 3
+    inp.t 0
+    inpx 2
+    outp.t 2
+    out 0, r3
+    inp 2
+    inp 3
+    outn 3, lr
+    inp 0
+    outp 2
+    in r8, 2
+    outn 3, zr
+    inp 0
+    outp 0
+    inp 0
+    out 0, 0x6031
+    in zr, 0
+    out 1, r8
+    inpx 2
+    inp.t 3
+    out 1, r7
+    in r9, 2
+    inp 2
+    in r8, 1
+    outn 0, r13
+    outn 2, r6
+    out 3, r2
+    inpx 2
+    inp.t 3
+    inp 3
+    outn 1, r7
+    inp 1
+    outp 0
+    inp 3
+    in r11, 3
+    outp 3
+    outp 2
+    inpx 1
+    out.t 3, r11
+    inp 0
+    out 2, r11
+    outn 1, r10
+    outn 2, r13
+    out 1, r5
+    out 1, 0x8eca
+    out 1, r9
+    in r4, 1
+    out 1, r13
+    outn 1, r7
+    in r1, 3
+    outp 1
+    in r2, 2
+    outn 2, r7
+    in r10, 1
+    outn 0, r8
+    out 1, r13
+    inpx 1
+    out.t 1, r3
+    out 2, r3
+    outn 3, r3
+    outn 0, r3
+    inp 1
+    inp 1
+    out 1, r13
+    outn 3, 0xf259
+    inpx 1
+    inp.t 3
+    in lr, 2
+    in r2, 2
+    inp 1
+    outn 0, r13
+    inp 0
+    in r10, 2
+    outp 2
+    outn 0, r7
+    inpx 3
+    out.t 2, r1
+    in r7, 2
+    inp 3
+    outn 3, r6
+    outp 2
+    inp 3
+    inpx 0
+    outp.t 3
+    inp 2
+    inp 0
+    outp 1
+    inp 0
+    inp 3
+    out 3, r12
+    inp 3
+    in r1, 1
+    outp 1
+    inpx 2
+    outn.t 1, r6
+    outn 3, 0xe38d
+    out 0, r3
+    outp 3
+    inpx 1
+    in.t r11, 3
+    outp 2
+    in sp, 3
+    outp 1
+    inpx 3
+    outp.t 3
+    inp 2
+    inpx 3
+    in.t r12, 1
+    inpx 3
+    out.t 3, r11
+    outp 3
+    inpx 1
+    outn.t 0, r3
+    in r5, 0
+    outn 0, lr
+    inp 2
+    outn 1, r2
+    out 0, r8
+    outp 0
+    inp 2
+    outn 3, r9
+    inpx 3
+    out.t 3, r8
+    in r7, 2
+    out 1, lr
+    inpx 1
+    out.t 2, r12
+    outn 2, r3
+    out 1, r5
+    inpx 3
+    outn.t 1, r3
+    outn 0, r11
+    inpx 2
+    out.t 0, r13
+    inp 0
+    out 3, r9
+    in lr, 3
+    out 3, r2
+    out 2, zr
+    outn 0, r7
+    in r7, 3
+    inp 1
+    outp 3
+    inp 2
+    outn 3, r2
+    out 1, r2
+    inp 2
+    inp 2
+    outn 2, r5
+    in r8, 0
+    outn 2, lr
+    inpx 0
+    in.t r1, 0
+    inp 3
+    out 3, r10
+    in r13, 3
+    inpx 2
+    in.t r13, 2
+    outp 0
+    in r7, 1
+    inpx 3
+    inp.t 3
+    outn 0, r5
+    outn 0, r1
+    outp 2
+    in r13, 3
+    inp 0
+    inpx 0
+    outp.t 0
+    outp 2
+    inp 1
+    in r12, 2
+    in r7, 3
+    in r11, 2
+    out 0, r7
+    out 1, zr
+    inp 2
+    in lr, 3
+    in r6, 1
+    in r13, 1
+    out 0, r8
+    inp 1
+    in r4, 3
+    outp 0
+    inp 0
+    outn 0, r1
+    inpx 1
+    out.t 0, r12
+    in r12, 1
+    inpx 0
+    outn.t 3, r2
+    outp 0
+    inpx 3
+    in.t r12, 0
+    inp 0
+    in lr, 2
+    inpx 1
+    outp.t 2
+    in r11, 1
+    inp 2
+    outn 3, r6
+    out 3, zr
+    inp 3
+    in r9, 0
+    inp 3
+    inpx 0
+    in.t r2, 1
+    in r13, 3
+    in zr, 0
+    outp 2
+    outp 2
+    outp 2
+    in r9, 3
+    outp 1
+    out 2, lr
+    inpx 0
+    out.t 1, r3
+    inp 3
+    inp 3
+    outp 0
+    outn 3, r7
+    inp 1
+    out 0, zr
+    inpx 3
+    outn.t 3, lr
+    out 2, 0x21a9
+    inp 3
+    out 2, 0x188a
+    inpx 1
+    out.t 3, lr
+    inp 1
+    in r2, 2
+    in r1, 3
+    inp 2
+    in r5, 0
+    inpx 0
+    inp.t 1
+    outp 2
+    inp 2
+    in r8, 2
+    outn 3, 0x5b3
+    inpx 3
+    inp.t 0
+    outn 2, r2
+    out 2, r7
+    outp 1
+    outn 3, r11
+    outn 2, lr
+    outn 1, r10
+    outp 1
+    inpx 1
+    inp.t 2
+    inp 0
+    inp 1
+    outn 3, r10
+    in r10, 1
+    outp 2
+    inp 0
+    inpx 0
+    out.t 3, r3
+    out 2, r13
+    inpx 2
+    inp.t 3
+    inpx 3
+    in.t sp, 2
+    inp 3
+    in r1, 3
+    outp 0
+    inpx 2
+    inp.t 3
+    in r9, 1
+    out 1, r4
+    outn 1, r11
+    out 1, lr
+    outp 3
+    in r4, 0
+    out 2, r6
+    inpx 0
+    outp.t 2
+    in r13, 2
+    inp 1
+    out 0, r5
+    outn 2, r1
+    inpx 0
+    out.t 2, r5
+    in r12, 1
+    inp 2
+    out 0, r2
+    out 2, r3
+    inp 2
+    outp 1
+    inpx 3
+    inp.t 3
+    inpx 3
+    inp.t 1
+    inpx 3
+    out.t 0, r6
+    out 1, r11
+    outp 2
+    outn 0, r10
+    out 1, r10
+    in r6, 2
+    outn 2, r3
+    inpx 3
+    in.t r6, 1
+    outn 0, r6
+    out 0, r11
+    inpx 3
+    outp.t 2
+    outn 0, r13
+    out 1, r5
+    out 1, r2
+    inp 2
+    inp 2
+    in r7, 1
+    inp 0
+    outn 3, r11
+    in lr, 0
+    out 0, r9
+    out 1, r4
+    outn 3, 0xc39d
+    outp 1
+    in r3, 1
+    in r13, 1
+    out 2, r7
+    out 3, r11
+    out 2, lr
+    out 3, r12
+    out 3, r2
+    outn 0, r8
+    inp 2
+    inp 1
+    inp 1
+    inpx 3
+    in.t sp, 2
+    inpx 1
+    out.t 2, zr
+    outp 3
+    out 3, r1
+    outp 2
+    inp 2
+    in r2, 1
+    in r2, 2
+    outn 3, r3
+    outp 2
+    out 3, r13
+    out 0, r1
+    inpx 2
+    out.t 3, r3
+    outp 0
+    in r11, 1
+    outp 3
+    outn 1, zr
+    outp 0
+    in r9, 0
+    inp 1
+    inpx 2
+    outn.t 1, r9
+    in sp, 3
+    inp 0
+    inp 1
+    outn 0, 0x2302
+    out 0, zr
+    outp 0
+    inp 0
+    inp 1
+    inpx 3
+    out.t 2, r7
+    out 0, r8
+    outn 3, r8
+    outp 2
+    inpx 0
+    outp.t 1
+    in r12, 1
+    outn 2, r12
+    outn 3, r4
+    outn 0, r5
+    outp 2
+    outp 1
+    out 3, r8
+    in r9, 0
+    out 3, r2
+    outn 3, r6
+    inpx 1
+    in.t r8, 3
+    inpx 0
+    outp.t 0
+    outp 3
+    outp 0
+    out 3, r12
+    inpx 0
+    outp.t 3
+    inpx 3
+    out.t 2, r6
+    outp 2
+    inp 0
+    out 0, lr
+    in r3, 0
+    inpx 2
+    out.t 2, r3
+    outp 0
+    out 1, r3
+    inpx 0
+    outn.t 0, r11
+    inpx 3
+    outn.t 1, r9
+    in r6, 3
+    outn 1, r6
+    out 1, r9
+    outn 0, r7
+    inp 3
+    outn 3, r5
+    outn 2, 0xe4bd
+    out 1, r12
+    inpx 1
+    in.t r4, 3
+    in r4, 0
+    in r9, 0
+    inp 3
+    inpx 2
+    out.t 0, r13
+    outn 0, r9
+    in r1, 2
+    outn 1, r7
+    inpx 2
+    in.t r3, 2
+    inpx 2
+    inp.t 1
+    in r8, 3
+    outp 0
+    outp 3
+    inp 1
+    outn 0, r13
+    outn 3, r8
+    out 1, r5
+    out 0, r12
+    inp 2
+    outp 2
+    inpx 3
+    out.t 0, r10
+    out 2, lr
+    outn 0, r11
+    outp 3
+    outn 2, r6
+    inp 1
+    outp 2
+    outp 3
+    outp 0
+    inp 0
+    inp 0
+    inpx 0
+    inp.t 1
+    in r2, 0
+    inpx 3
+    outp.t 1
+    outp 3
+    outp 2
+    inpx 3
+    outp.t 0
+    outn 1, r10
+    inpx 3
+    out.t 2, r8
+    outp 2
+    inpx 1
+    inp.t 2
+    inpx 0
+    inp.t 1
+    outn 2, r4
+    out 2, r1
+    outn 3, r5
+    in r10, 3
+    out 2, 0xb24a
+    inp 3
+    outn 1, lr
+    outp 3
+    outn 0, r10
+    outp 2
+    outp 1
+    outp 0
+    inp 2
+    out 0, r5
+    inpx 1
+    outp.t 1
+    out 2, r10
+    outn 2, r6
+    outp 2
+    outp 1
+    out 1, r10
+    outp 0
+    outn 2, r10
+    outp 1
+    outn 3, r8
+    outp 2
+    out 2, r4
+    outp 1
+    inp 2
+    in r10, 2
+    inpx 2
+    outn.t 0, 0x9067
+    outn 2, r7
+    outp 0
+    in r12, 3
+    in r1, 1
+    outp 2
+    outp 1
+    outn 1, r6
+    inp 3
+    inpx 2
+    in.t r3, 2
+    inpx 0
+    out.t 0, r12
+    out 1, r11
+    out 2, r5
+    inpx 1
+    outn.t 3, r12
+    inp 3
+    out 3, r13
+    outp 1
+    inp 3
+    outn 3, r5
+    inp 3
+    outp 1
+    out 2, 0x2e63
+    in r12, 2
+    outp 0
+    inp 2
+    inpx 2
+    in.t r5, 3
+    outn 3, r3
+    inpx 3
+    in.t r7, 3
+    out 2, r13
+    out 2, r9
+    in r1, 3
+    in r11, 0
+    out 2, r3
+    inpx 2
+    outn.t 3, zr
+    outn 3, r8
+    inpx 0
+    out.t 0, r11
+    in r3, 2
+    in r3, 2
+    outn 2, r2
+    out 1, r2
+    inp 2
+    inpx 3
+    inp.t 0
+    inp 2
+    outn 0, lr
+    inp 0
+    outn 2, r4
+    in r9, 0
+    outn 0, lr
+    out 3, r13
+    inp 3
+    out 1, 0xb58d
+    out 0, r4
+    inpx 3
+    out.t 1, r6
+    outp 3
+    inp 3
+    outn 1, 0xf187
+    outp 0
+    in r12, 2
+    outn 0, r10
+    in r13, 1
+    in lr, 2
+    in lr, 1
+    inpx 1
+    inp.t 3
+    outp 0
+    inpx 3
+    outn.t 2, 0x9529
+    inpx 2
+    in.t r7, 0
+    in r1, 0
+    out 0, 0xf7e4
+    inpx 1
+    in.t r7, 3
+    out 0, r12
+    outp 0
+    inp 3
+    in r5, 0
+    outn 2, r12
+    outp 3
+    outp 2
+    outn 0, r10
+    in r5, 3
+    outn 1, r4
+    in r11, 1
+    outn 0, r8
+    inp 2
+    outp 1
+    in r13, 0
+    out 0, zr
+    outp 3
+    in sp, 1
+    inp 0
+    outp 1
+    outn 1, r6
+    inp 3
+    outn 3, r12
+    inp 0
+    out 2, r1
+    inpx 3
+    out.t 2, lr
+    inp 1
+    inpx 0
+    inp.t 2
+    inp 2
+    out 1, r5
+    outn 2, r2
+    inp 3
+    inpx 1
+    inp.t 0
+    in r1, 1
+    in r13, 1
+    out 0, r1
+    in r4, 2
+    outp 3
+    outn 1, lr
+    inp 1
+    in r6, 3
+    outp 1
+    out 0, r12
+    out 3, r11
+    in r3, 3
+    in zr, 2
+    out 1, r2
+    in r7, 1
+    inp 1
+    outp 3
+    inp 3
+    inp 3
+    in r1, 0
+    outn 3, r7
+    in r12, 3
+    outn 2, r1
+    outp 1
+    inp 2
+    inp 1
+    inp 3
+    inpx 0
+    outp.t 1
+    inp 3
+    in r2, 2
+    out 1, r6
+    inp 2
+    inp 0
+    out 3, lr
+    outn 2, r5
+    in r5, 0
+    out 0, r2
+    out 3, r12
+    inpx 0
+    add.t zr, zr, zr
+    utx 0x40
+    utx 0x40
+    utx 0x45
+    utx 0x4e
+    utx 0x44
+    utx 0x40
+    utx 0x40
+    utx zr
+    b 0xffff

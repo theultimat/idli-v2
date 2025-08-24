81bb8f4af842e2091332ae78e44f0922593f9527
theultimat <theultimat@gmail.com>
2025-08-24 14:09:20 +0100

    scripts: Print out test name during regression.

--- scripts/regress.sh
+++ scripts/regress.sh
@@ -14,6 +14,10 @@ make -j8 sv2v
 TESTS=$(find build/asm -type f -name '*.out')
 
 for TEST in $TESTS; do
+    echo "===================="
+    echo "$TEST"
+    echo "===================="
+
     make -j8 run_sim SIM_TEST="$TEST"
     make -j8 run_veri SIM_TEST="$TEST"
     make -j8 run_icarus SIM_TEST="$TEST"
